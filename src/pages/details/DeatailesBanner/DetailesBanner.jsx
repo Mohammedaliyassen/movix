09e86150f7cc7f309847e90d753859685305ab1a
Mohammedaliyassen <73065131+Mohammedaliyassen@users.noreply.github.com>
2023-10-11 02:17:07 +0300

    Update DetailesBanner.jsx

--- src/pages/details/DeatailesBanner/DetailesBanner.jsx
+++ src/pages/details/DeatailesBanner/DetailesBanner.jsx
@@ -11,7 +11,7 @@ import Genres from "../../../components/Geners/Geners";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
-import { PlayIcon } from "../PlayBtn";
+import { PlayIcon } from "../PlayBtn.jsx";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
